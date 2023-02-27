82d4f8d1e0b9c45b9428e000ffa7a77de2dcc102
JosCha94 <72150891+JosCha94@users.noreply.github.com>
2023-02-27 15:05:14 -0500

    traer datos y mostrarlos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,44 @@
 <script>
+  import { json } from "@sveltejs/kit";
   import "bootstrap/dist/css/bootstrap.min.css";
   import "bootstrap/dist/js/bootstrap.min.js";
 
-  
+  /**
+   * @type {any[]}
+   */
+  let empleados = [];
+  let datosEmpleado = {
+    id: null,
+    nombre: "",
+    correo: "",
+  };
+
+  let mostrarEmpleados = () => {
+    fetch("http://localhost/API%20Empleados/")
+      .then((respuesta) => respuesta.json())
+      .then((datosRespuesta) => {
+        empleados = datosRespuesta;
+
+        datosRespuesta = {
+          id: null,
+          nombre: "",
+          correo: "",
+        };
+        console.log(empleados);
+      })
+      .catch(console.log);
+  };
+
+  mostrarEmpleados();
 </script>
 
 <div class="container my-3">
-    <div class="row gap-xs-3">
-        <h1 class="text-center mb-4">API DE EMPLEADOS <span class="h3"> con svelte</span></h1>
-  <div class="col-12 col-sm-6">
-    <div class="card">
+  <div class="row gap-xs-3">
+    <h1 class="text-center mb-4">
+      API DE EMPLEADOS <span class="h3"> con svelte</span>
+    </h1>
+    <div class="col-12 col-sm-6">
+      <div class="card">
         <div class="card-header">Empleados</div>
         <div class="card-body">
           <form>
@@ -21,43 +50,38 @@
               <label for="Correo" class="form-label">Correo</label>
               <input type="email" class="form-control" id="Correo" />
             </div>
-  
-            <button type="button" class="btn btn-primary">Agregar Empleado</button>
-            <button type="button" class="btn btn-primary">Actualizar Empleado</button>
+
+            <button type="button" class="btn btn-primary"
+              >Agregar Empleado</button
+            >
+            <button type="button" class="btn btn-primary"
+              >Actualizar Empleado</button
+            >
           </form>
         </div>
       </div>
+    </div>
+    <div class="col-12 col-sm-6">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Correo</th>
+            <th scope="col">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {#each empleados as empleado}
+            <tr>
+              <th>{empleado.id}</th>
+              <td>{empleado.nombre}</td>
+              <td>{empleado.correo}</td>
+              <td><button class="btn btn-warning" type="submit">Editar</button> | <button class="btn btn-danger" type="submit">Borrar</button></td>
+            </tr>
+          {/each}
+        </tbody>
+      </table>
+    </div>
   </div>
-  <div class="col-12 col-sm-6">
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">ID</th>
-          <th scope="col">Nombre</th>
-          <th scope="col">Correo</th>
-          <th scope="col">Acciones</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th>1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th>2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th>3</th>
-          <td>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</div>
 </div>
